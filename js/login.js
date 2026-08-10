473488661d657fdaaf0b53206973332ffac544c5
Cynthia <parmarvaidehi437@gmail.com>
2026-08-10 14:07:12 +0530

    Basic Regex

---
+++ js/login.js
@@ -0,0 +1,3 @@
+const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+$/;
+
+const passwordRegex = /^[a-zA-Z0-9]{8,}$/;
---
+++ js/register.js
@@ -0,0 +1,2 @@
+const form = document.getElementById("registerForm");
+const studentIdRegex = /^\d{2}[A-Z]{3}\d{3}$/;
